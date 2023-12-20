0c9405072a21b65f16db6e842e8bc35597afe850
Sol0player <darien.booker@geogroup.com>
2023-12-20 13:26:18 -0500

    Home Lap Top Set Up

---
+++ Chapter 1/Listings/ShowSyntaxErrors.java
@@ -0,0 +1,10 @@
+//Listing 1.4
+/*
+public class ShowSyntaxErrors {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java ); //Missing the parenthesis
+    }
+}
+*/
+
+//Uncomment and run to see the error
