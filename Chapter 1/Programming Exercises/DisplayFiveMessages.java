59854bb06b406395ec6fad550a566a04c0421e63
sol0player <darien.booker@geogroup.com>
2023-12-21 10:43:03 -0500

    12/21/23

---
+++ Chapter 1/Programming Exercises/DisplayFiveMessages.java
@@ -0,0 +1,14 @@
+/*
+ * Write a program that displays Welcome to Java five times.
+ * Programming Excersise 1.2 */
+
+
+public class DisplayFiveMessages {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+    }
+}
---
+++ Chapter 1/Programming Exercises/DisplayThreeMessages.java
@@ -0,0 +1,12 @@
+/*
+ * Display Three Messages
+ * Programming Excercise 1.1
+ */
+
+public class DisplayThreeMessages {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Programming is fun");
+    }
+}
